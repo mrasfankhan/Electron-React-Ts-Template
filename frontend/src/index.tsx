c96c8accfa8d8cb7059b02b7713db6e1043fc23f
asfandyar khan <mrasfankhan@gmail.com>
2022-07-17 20:15:41 -0400

    Stating template

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,9 +1,8 @@
-// @tsx-ignore
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App.tsx';
-import reportWebVitals from './reportWebVitals.ts';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
